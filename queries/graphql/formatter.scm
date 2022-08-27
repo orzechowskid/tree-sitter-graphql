d095abb803119578f29cff70104160373c10bf3c
Dan Orzechowski <orzechod@gmail.com>
2022-08-26 23:38:52 -0400

    remove apparently busted queries

--- queries/graphql/formatter.scm
+++ queries/graphql/formatter.scm
@@ -2,5 +2,3 @@
 (fields_definition (field_definition) @field_definition)
 (fields_definition (field_definition (name) @name)) 
 (input_object_type_definition) @fields_definition
-(input_object_type_definition (input_value_definition) @field_definition)
-(input_object_type_definition (input_value_definition (name) @name)) 
